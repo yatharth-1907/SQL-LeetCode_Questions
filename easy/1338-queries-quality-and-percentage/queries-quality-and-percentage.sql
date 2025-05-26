ea25c2198bdc8db110fcc00b750fa35f7af12c3a
Yatharth Patankar <yatharthpatankar07@gmail.com>
2025-05-26 10:33:56 +0530

    Time: 403 ms (23.74%) | Memory: 0B (100.00%) - LeetSync

--- easy/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ easy/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
-Select query_name, ROUND(avg(rating/position),2) as quality, ROUND(AVG(rating<3)*100,2) as poor_query_percentage
+Select 
+    query_name, 
+    ROUND(avg(rating/position),2) as quality, 
+    ROUND(AVG(rating<3)*100,2) as poor_query_percentage
 FROM Queries 
 group by query_name
